058d2128a4ffcbe7f74f2172bb09592a176d2aff
Adrian Skelton <adrianskelton9@gmail.com>
2024-01-13 13:51:55 +0000

    fix models.py

--- recipe/models.py
+++ recipe/models.py
@@ -4,12 +4,11 @@ from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
 from django.core.validators import MaxValueValidator
 
-
 class Recipe(models.Model):
     """
     My Recipe model to publish recipes
     """
-    author = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, default=1) 
     date = models.DateTimeField(auto_now_add=True)
     title = models.CharField(max_length=100)
     country = models.CharField(max_length=100)
@@ -18,15 +17,13 @@ class Recipe(models.Model):
     ingredients = models.TextField()
     instructions = models.TextField()
     servings = models.PositiveIntegerField(validators=[MaxValueValidator(20)])
-    likes = models.ManyToManyField(User, related_name='recipe_likes',
-                                   blank=True)
+    likes = models.ManyToManyField(User, related_name='recipe_likes', blank=True)
 
     class Meta:
         ordering = ["-date"]
-
+        
     def __str__(self):
         return self.title
-
 
 class Like_model(models.Model):
     title = models.CharField(max_length=255)
@@ -34,13 +31,8 @@ class Like_model(models.Model):
     likes = models.ManyToManyField(User, related_name='liked_recipes')
     dislikes = models.ManyToManyField(User, related_name='disliked_recipes')
 
-def __str__(self):
-    return (
-        f"{self.title} - "
-        f"Likes: {self.likes.count()}, "
-        f"Dislikes: {self.dislikes.count()}"
-    )
-
+    def __str__(self):
+        return f"{self.title} - Likes: {self.likes.count()}, Dislikes: {self.dislikes.count()}"
 
 class Comment(models.Model):
     email = models.EmailField()
