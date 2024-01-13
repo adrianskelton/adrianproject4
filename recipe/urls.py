0dc4f9f146d021c4964d7f17e3805196f4a0a4bb
Adrian Skelton <adrianskelton9@gmail.com>
2024-01-13 15:08:56 +0000

    refactor urls.py

--- recipe/urls.py
+++ recipe/urls.py
@@ -1,13 +1,16 @@
-from django.urls import path
-from .views import home, recipe_view, create_or_edit_recipe, recipe_detail, user_recipes, like_recipe, unlike_recipe, top_five, sort_by_country, latest_recipes, all_recipes, add_comment, delete_recipe
-
 from . import views
+from django.urls import path
+from .views import home, recipe_view, create_or_edit_recipe, recipe_detail,
+user_recipes, like_recipe, unlike_recipe, top_five, sort_by_country,
+latest_recipes, all_recipes, add_comment, delete_recipe
+
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('recipe/', recipe_view, name='recipe'),
     path('create-recipe/', create_or_edit_recipe, name='create_recipe'),
-    path('edit-recipe/<int:pk>/', create_or_edit_recipe, name='edit_recipe'),    path('like_recipe/<int:pk>/', like_recipe, name='like_recipe'),
+    path('edit-recipe/<int:pk>/', create_or_edit_recipe, name='edit_recipe'),
+    path('like_recipe/<int:pk>/', like_recipe, name='like_recipe'),
     path('unlike_recipe/<int:pk>/', unlike_recipe, name='unlike_recipe'),
     path('recipe/<int:pk>/', recipe_detail, name='recipe_detail'),
     path('all-recipes/', all_recipes, name='all_recipes'),
@@ -15,6 +18,7 @@ urlpatterns = [
     path('latest-recipes/', latest_recipes, name='latest_recipes'),
     path('sort-by-country/', sort_by_country, name='sort_by_country'),
     path('user-recipes/', user_recipes, name='user_recipes'),
-    path('recipe/<int:recipe_id>/add_comment/', add_comment, name='add_comment'),
+    path('recipe/<int:recipe_id>/add_comment/', add_comment,
+         name='add_comment'),
     path('delete_recipe/<int:pk>/', delete_recipe, name='delete_recipe'),
 ]
